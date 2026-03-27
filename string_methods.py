e8c8ec0e584403c320790a091b892a143762b132
emiliakoron <ekoron@mail.austral.edu.ar>
2026-03-27 20:16:20 +0100

    Update string_methods.py

--- string_methods.py
+++ string_methods.py
@@ -8,3 +8,27 @@ def string_methods():
     multilinea = """Linea 1
     Linea 2
     Linea 3"""
+    print(nombre.strip())
+    print(nombre.lstrip())
+    print(nombre.rstrip())
+
+    print(frase.upper())
+    print(frase.lower())
+    print(frase.title())
+
+    print(frase.find("gran"))
+    print(frase.replace("programacion", "desarrollo"))
+
+    print(frase.count("a"))
+    print("Python" in frase)
+    print("Java" in frase)
+
+    print(frase[0:6])
+    print(frase[0:6:2])
+    print(frase[0:6][::-1])
+
+    nombre_sin_espacios = nombre.strip()
+    python = frase[0:6]
+    print(f"{nombre_sin_espacios} trabaja con {python}")
+
+    print(multilinea)
