e94f4b373ebcd8281788f2dad7e0b3451d3676aa
emiliakoron <ekoron@mail.austral.edu.ar>
2026-03-27 21:47:32 +0100

    Update ficha.py

--- ficha.py
+++ ficha.py
@@ -25,3 +25,59 @@ def ficha():
     #   - Las 3 notas, su suma, promedio y promedio entero
     #   - Cierre decorativo usando repetición de string ("=" * 24)
     pass
+    nombre_completo = input()
+    email = input()
+    nota1 = input()
+    nota2 = input()
+    nota3 = input()
+
+    #encabezado decorativo
+    print("=" * 24)
+    print("FICHA DEL ALUMNO")
+    print("=" * 24)
+
+    nombre_limpio = nombre_completo.strip().title()
+    print("Nombre: ", nombre_limpio)
+
+    email_limpio = email.strip().lower()
+    print("Email: ", email_limpio)
+
+    cantidad = len(nombre_limpio)
+    print("Caracteres en nombre: ", cantidad)
+
+    pos_espacio = nombre_limpio.find(" ")
+    iniciales = nombre_limpio[0] + nombre_limpio[pos_espacio + 1 ]
+    print("Iniciales: ", iniciales)
+
+    nombre_en_partes = nombre_limpio.split()
+    usuario = f"{nombre_en_partes[-1].lower()}.{nombre_en_partes[0].lower()}"
+    print("Usuario: ", usuario)
+
+    print("Email valido: ", "@" in email_limpio)
+
+    dominio = email_limpio[email_limpio.find("@") + 1:]
+    print("Dominio: ", dominio)
+
+    nombre_guion = nombre_limpio.replace(" ", "_")
+    print("Nombre para archivo: ", nombre_guion)
+
+    print("Cantidad de a: ", nombre_limpio.count("a"))
+
+    print("Codigo secreto: ", nombre_limpio[::-1].upper())
+
+    n1 = float(nota1)
+    n2 = float(nota2)
+    n3 = float(nota3)
+
+    suma = n1 + n2 + n3
+    promedio = suma / 3
+    promedio_entero = int(promedio)
+
+    print("Nota 1: ", n1)
+    print("Nota 2 ", n2)
+    print("Nota 3", n3)
+    print("Suma: ", suma)
+    print("Promedio: ", promedio)
+    print("Promedio_entero: ", promedio_entero)
+
+    print("="*24)
